6c0a0fa95442666edd169f624612cd391d2be282
BJ <bjbueno@cultbranding.com>
2024-05-06 16:48:32 -0400

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by BJ Bueno on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
